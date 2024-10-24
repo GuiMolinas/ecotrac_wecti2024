179068630b95ff9175705c04246be3ce313cfdf9
JJDelmiro <132026425+JJDelmiro@users.noreply.github.com>
2024-10-24 20:49:46 -0300

    bom dia

--- app/src/main/java/com/wecti/unicid/myapplication/Splash.java
+++ app/src/main/java/com/wecti/unicid/myapplication/Splash.java
@@ -12,6 +12,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+/* bom dia - jj */
+
 public class Splash extends AppCompatActivity {
 
     @Override
