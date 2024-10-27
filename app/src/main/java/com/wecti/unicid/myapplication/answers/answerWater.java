fdb48e1680832d310854196008f0ec33fe309e5c
GuiMolinas <104726078+GuiMolinas@users.noreply.github.com>
2024-10-27 11:15:50 -0300

    Water

---
+++ app/src/main/java/com/wecti/unicid/myapplication/answers/answerWater.java
@@ -0,0 +1,72 @@
+package com.wecti.unicid.myapplication.answers;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.wecti.unicid.myapplication.R;
+
+public class answerWater extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView txtTotal, txtResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_answer_water);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        imageView = findViewById(R.id.imgRabbit);
+
+        Glide.with(this)
+                .load(R.drawable.rabbit)
+                .into(imageView);
+
+        double total = getIntent().getDoubleExtra("consumo_total", 0);
+
+        double consumoMaquina = getIntent().getDoubleExtra("consumo_maquina", 0);
+        double consumoTanque = getIntent().getDoubleExtra("consumo_tanque", 0);
+        double consumoDescarga = getIntent().getDoubleExtra("consumo_descarga", 0);
+        double consumoPia = getIntent().getDoubleExtra("consumo_pia", 0);
+        double consumoChuveiro = getIntent().getDoubleExtra("consumo_chuveiro", 0);
+        double consumoCozinha = getIntent().getDoubleExtra("consumo_cozinha", 0);
+        double consumoLavaLouca = getIntent().getDoubleExtra("consumo_lavaLouca", 0);
+
+        txtTotal = findViewById(R.id.txtTotal);
+        txtResultado = findViewById(R.id.txtResultado);
+
+        // Exibe os valores de consumo individual nos TextView correspondentes
+        ((TextView) findViewById(R.id.txtConsumoMaquina)).setText("Máquina de Lavar: " + consumoMaquina + "L");
+        ((TextView) findViewById(R.id.txtConsumoTanque)).setText("Tanque: " + consumoTanque + "L");
+        ((TextView) findViewById(R.id.txtConsumoDescarga)).setText("Descarga: " + consumoDescarga + "L");
+        ((TextView) findViewById(R.id.txtConsumoBanheiro)).setText("Pia Banheiro: " + consumoPia + "L");
+        ((TextView) findViewById(R.id.txtConsumoChuveiro)).setText("Chuveiro: " + consumoChuveiro + "L");
+        ((TextView) findViewById(R.id.txtConsumoCozinha)).setText("Pia Cozinha: " + consumoCozinha + "L");
+        ((TextView) findViewById(R.id.txtConsumoLavaLouca)).setText("Lava Louças: " + consumoLavaLouca + "L");
+
+        txtTotal.setText("Consumo Total - " + total + "L");
+
+        // Verifica se ultrapassou o limite e define a conclusão
+        if (total > 110) {
+            double excesso = total - 110;
+            double porcentagem = (excesso / 110) * 100;
+            txtResultado.setText("Você ultrapassou " + String.format("%.2f", excesso) +
+                    "L do recomendado (" + String.format("%.2f", porcentagem) + "%)");
+        } else {
+            txtResultado.setText("Consumo dentro do recomendado");
+        }
+    }
+}
--- app/src/main/java/com/wecti/unicid/myapplication/asks/askWater.java
+++ app/src/main/java/com/wecti/unicid/myapplication/asks/askWater.java
@@ -1,9 +1,13 @@
 package com.wecti.unicid.myapplication.asks;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,11 +17,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.bumptech.glide.Glide;
 import com.wecti.unicid.myapplication.R;
+import com.wecti.unicid.myapplication.answers.answerWater;
 
 public class askWater extends AppCompatActivity {
 
     ImageView imageView;
     ImageButton back;
+
+    EditText edtMaquina, edtTanque, edtDescarga, edtPia, edtChuveiro,
+            edtCozinha, edtLavaLouca;
+
+    Button btnEnviarDados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +43,16 @@ public class askWater extends AppCompatActivity {
         imageView = findViewById(R.id.imgWater);
         back = findViewById(R.id.btnVoltar);
 
+        edtMaquina = findViewById(R.id.edtMaquina);
+        edtTanque = findViewById(R.id.edtTanque);
+        edtDescarga = findViewById(R.id.edtDescarga);
+        edtPia = findViewById(R.id.edtPia);
+        edtChuveiro = findViewById(R.id.edtChuveiro);
+        edtCozinha = findViewById(R.id.edtCozinha);
+        edtLavaLouca = findViewById(R.id.edtLavaLouca);
+
+        btnEnviarDados = findViewById(R.id.btnEnviarDados);
+
         Glide.with(this)
                 .load(R.drawable.water)
                 .into(imageView);
@@ -43,5 +63,42 @@ public class askWater extends AppCompatActivity {
                 finish();
             }
         });
+
+        btnEnviarDados.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double consumoMaquina = obterValor(edtMaquina) * 19;
+                double consumoTanque = obterValor(edtTanque) * 15;
+                double consumoDescarga = obterValor(edtDescarga) * 6;
+                double consumoPia = obterValor(edtPia) * 15;
+                double consumoChuveiro = obterValor(edtChuveiro) * 12;
+                double consumoCozinha = obterValor(edtCozinha) * 15;
+                double consumoLavaLouca = obterValor(edtLavaLouca) * 2;
+
+                double total = consumoMaquina + consumoTanque + consumoDescarga + consumoPia
+                        + consumoChuveiro + consumoCozinha + consumoLavaLouca;
+
+                Intent intent = new Intent(askWater.this, answerWater.class);
+                intent.putExtra("consumo_total", total);
+
+                intent.putExtra("consumo_maquina", consumoMaquina);
+                intent.putExtra("consumo_tanque", consumoTanque);
+                intent.putExtra("consumo_descarga", consumoDescarga);
+                intent.putExtra("consumo_pia", consumoPia);
+                intent.putExtra("consumo_chuveiro", consumoChuveiro);
+                intent.putExtra("consumo_cozinha", consumoCozinha);
+                intent.putExtra("consumo_lavaLouca", consumoLavaLouca);
+                startActivity(intent);
+
+                //Teste
+                //Toast.makeText(askWater.this, "Consumo diário: " + total, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    private double obterValor(EditText edtText) {
+        String valor = edtText.getText().toString();
+        return valor.isEmpty() ? 0 : Double.parseDouble(valor);
+    }
+
 }
